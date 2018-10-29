5cf131831d5f800344b8e4415d82ab78c1200935
Joshua <obasajujoshua31@gmail.com>
2018-10-29 17:25:51 +0100

    added url logging to the project

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var http = require('http');
-
+var path = require('path');
 var server = http.createServer();
 
+
+var url = require('url');
+var urlpath = path.join(__dirname, urlpath);
 server.listen(4000, function(){
 
 	console.log("server started at 4000")
 })
+
+
