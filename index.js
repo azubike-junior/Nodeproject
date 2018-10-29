cc1dc83de9e5340f09522e1cc14cf431e964e02a
Joshua <obasajujoshua31@gmail.com>
2018-10-29 17:32:10 +0100

    added query logging

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var http = require('http');
 var path = require('path');
 var server = http.createServer();
 
-
+var query = urlquery;
+console.log(query);
 var url = require('url');
 var urlpath = path.join(__dirname, urlpath);
 server.listen(4000, function(){
